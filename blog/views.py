0275095c6d8c11a8b669e2f188e7c1b64d8f8a1c
dnalink <ydy0329@dnalink.com>
2023-03-10 13:10:53 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 #blog/post_list.html 템플릿 보여줌
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #posts라는 쿼리셋 생성.
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
