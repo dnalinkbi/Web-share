b858280f1821741a7e68e09d2ba8054ff37c9804
dnalink <ydy0329@dnalink.com>
2023-03-10 15:20:16 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    #post/는 URL이 post 문자를 포함해야한다는 것을 의미
+    #<int:pk>는 장고는 int값을 받아 pk라는 변수로 뷰를 전달
+    #/는 다음에 /문자가 한번 더 와야한다.
+    #예를들어, http://127.0.0.1:8000/post/5/ 인 경우, 장고는 post_detail 뷰를 찾아 매기변수 pk가 5인 값을 찾아 뷰로 전달
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 #blog/post_list.html 템플릿 보여줌
 def post_list(request):
-    #posts라는 쿼리셋 생성.
+    #posts라는 쿼리셋 생성하여 Post 정렬된 데이터를 저장.
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+#만약 pk에 해당하는 post가 없으면 에러메시지를 그냥 출력하는 것이아니라 page not found 창을 출력하도록
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
